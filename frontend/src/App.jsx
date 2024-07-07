01eb810449c1184eaec8b9b3ce559bc519985048
Venu Akku <ivenu28@gmail.com>
2024-07-07 20:16:54 +0530

    made signup app home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,9 @@ import SendMoney from "./pages/SendMoney";
 function App() {
   return (
     <>
-    <h1>Go to <Link to={'/signup'}>Sign Up</Link> page</h1>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Signup />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />

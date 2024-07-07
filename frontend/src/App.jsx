6a382d8099aa6be56cfae2006c06353346120399
Venu Akku <ivenu28@gmail.com>
2024-07-07 20:07:32 +0530

    add h1 in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import SendMoney from "./pages/SendMoney";
 function App() {
   return (
     <>
+    <h1>Go to Sign Up page</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />

478a887a9f85d86bddaad86143294ce998efbf5c
Venu Akku <ivenu28@gmail.com>
2024-07-11 10:40:45 +0530

    update userSchema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     minLength: 2,
-    maxLength: 20,
+    maxLength: 100,
     trim: true,
     unique: true,
     lowercase: true,
@@ -13,25 +13,24 @@ const userSchema = mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minLength: 8,
+    minLength: 6,
     trim: true,
   },
   firstName: {
     type: String,
     required: true,
-    maxLength: 20,
+    maxLength: 50,
     trim: true,
   },
   lastName: {
     type: String,
     required: true,
-    maxLength: 20,
+    maxLength: 50,
     trim: true,
   },
   fullName: {
     type: String,
     required: true,
-    maxLength: 50,
     trim: true,
   },
 });
